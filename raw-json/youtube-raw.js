321db30b8c9a23f595dc97e2bd486e205093b382
Jerry Davidson <willdjerry@gmail.com>
2019-07-31 13:59:10 -0400

    youtube-raw.js shows concerts based off of artist name

--- raw-json/youtube-raw.js
+++ raw-json/youtube-raw.js
@@ -1,9 +1,14 @@
-// format
+// format for youtube links
 // 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=QUERY&key=[YOUR_API_KEY]' 
+
 const axios = require('axios')
 const fs = require('fs')
 
-let artistName = 'Jayz'
+// have to get each artist from ticketmaster json
+let artistName = null
 let query = `${artistName}%20concert`
-
-url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDeuyKWqo0zDyHhjXKY3VLQa0KcxXR-aAs`
+// url to get videos
+let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDeuyKWqo0zDyHhjXKY3VLQa0KcxXR-aAs`
+axios.get(url).then(response => {
+    console.log(JSON.stringify(response.data))
+})
