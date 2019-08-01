2d596300144301e205d15b83d74a127c5968d885
Jerry Davidson <willdjerry@gmail.com>
2019-07-31 22:19:59 -0400

    changed ticketmaster-raw.js to artists-raw.js

--- db/raw-json/youtube-raw.js
+++ db/raw-json/youtube-raw.js
@@ -4,20 +4,13 @@
 const axios = require('axios')
 const fs = require('fs')
 const Artist = require('../../models/Artists')
+
+
 // have to get each artist from Artist database
-
-
 Artist.find({}).then(artists => artists.forEach(artist =>{
     let query = `${artist.name}%20concert`
     let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDeuyKWqo0zDyHhjXKY3VLQa0KcxXR-aAs`
     axios.get(url).then(response => {
-        console.log(`${artist.name}:`,JSON.stringify(response.data))
+        console.log(JSON.stringify(response.data))
     })
 }))
-
-
-// let query = `${artistName}%20concert`
-// // url to get videos
-// axios.get(url).then(response => {
-//     console.log(JSON.stringify(response.data))
-// })
