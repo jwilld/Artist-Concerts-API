697698ee7f27209c8ef0fa74d4627ffef3d90577
Jerry Davidson <willdjerry@gmail.com>
2019-08-02 05:05:14 -0400

    deletion of genre, youtube links and artist added by sending a artists delete request by name

--- controllers/artists.js
+++ controllers/artists.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Artist = require("../models/Artists");
 const Youtube = require("../models/Youtube");
+const Genre = require("../models/Genre");
 
 router.get("/artists", (req, res) => {
   Artist.find({}).then(artists => res.json(artists));
@@ -16,7 +17,20 @@ router.post("/artists", (req, res) => {
   Artist.create(req.body).then(artist => res.json(artist));
 });
 
-router.delete('/artists/:name',(req,res) => {
-    Artist.findOneAndDelete({name: req.params.name}).then(res.send(`${req.params.name} deleted`))
-})
+router
+  .delete("/artists/:name", (req, res) => {
+    Artist.findOneAndDelete({ name: req.params.name }).then(
+      res.send(`${req.params.name} deleted from artists`)
+    );
+  })
+  .then(
+    Youtube.deleteMany({ name: req.params.name }).then(
+      res.send(`Deleted youtube links for ${req.params.name}`)
+    )
+  )
+  .then(
+    Genre.deleteMany({ name: req.params.name }).then(
+      res.send(`Deleted ${req.params.name}'s genre`)
+    )
+  );
 module.exports = router;
