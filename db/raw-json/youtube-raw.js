6e47119008d70a5a876152eb9e21afdb501527e9
Jerry Davidson <willdjerry@gmail.com>
2019-08-05 06:53:06 -0400

    removed api key

--- db/raw-json/youtube-raw.js
+++ db/raw-json/youtube-raw.js
@@ -4,12 +4,13 @@
 const axios = require('axios')
 const fs = require('fs')
 const Artist = require('../../models/Artists')
+const apiKey = ""
 
 
 // have to get each artist from Artist database
 Artist.find({}).then(artists => artists.forEach(artist =>{
     let query = `${artist.name}%20concert`
-    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDeuyKWqo0zDyHhjXKY3VLQa0KcxXR-aAs`
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}-aAs`
     axios.get(url).then(response => {
         console.log(JSON.stringify({"${artist.name}":response.data.items[0].id.videoId}))
     })
