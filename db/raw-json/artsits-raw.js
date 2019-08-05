f55ef6f52ecec9e9c88929a35a7744491f524c13
Jerry Davidson <willdjerry@gmail.com>
2019-08-05 06:54:35 -0400

    removed ticketmaster api key

--- db/raw-json/artsits-raw.js
+++ db/raw-json/artsits-raw.js
@@ -4,6 +4,7 @@
 
 const axios = require('axios');
 const fs = require('fs')
+const apiKey = ""
 
 //Make a request
 
@@ -14,7 +15,7 @@ const fs = require('fs')
 //             console.log(err)
 //         }})
 //     })
-axios.get("https://app.ticketmaster.com/discovery/v2/attractions.json?classificationName=Hip-Hop/Rap&apikey=nNYVKiR6NvxzGSf6qI2xZ7zdebBycfR6&size=10")
+axios.get(`https://app.ticketmaster.com/discovery/v2/attractions.json?classificationName=Hip-Hop/Rap&${apikey}==10`)
 .then(response =>{
     fs.writeFile('wiki.json',(JSON.stringify(response.data._embedded.attractions)),(err) =>{
         if(err){
