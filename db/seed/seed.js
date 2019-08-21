a7d4317aed25ce2ab84bd7a2d4d864598dfb132f
Jerry Davidson <willdjerry@gmail.com>
2019-08-21 07:54:16 -0400

    youtube-ids-2.json contains artist name and their links

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -7,9 +7,15 @@ const Hits = require("../../models/Hits");
 const hits = require("./genius-artists-hits.json");
 
 const Youtube = require("../../models/Youtube");
-const youtube = require("./youtube.json");
+const youtube = require("./youtube-ids-2.json");
 
 const Genre = require("../../models/Genre");
+
+
+youtube.forEach((response, i=0) => {
+    console.log(response.items[0].artist,response.items[0].id.videoId,`******${i}*****`)
+        return i+1
+    })
 
 // Artist.deleteMany({}).then(Artist.create(artists)).finally(()=> console.log('done seeding artists'))
 
@@ -22,21 +28,20 @@ const Genre = require("../../models/Genre");
 //   )
 // ).finally(console.log('done adding songs'))
 
-Artist.find({}).then(artists =>{
-    Hits.find({}).then(hit => {
-        artists.forEach(artist =>{
-            hit.forEach(hits =>{
-                if(artist.name === hits.primary_artist.name){
-                    artist.hits.push(hits)
-                }
-                artist.save()
-            })
-        })
-    })
-})
+// Artist.find({}).then(artists =>{
+//     Hits.find({}).then(hit => {
+//         artists.forEach(artist =>{
+//             hit.forEach(hits =>{
+//                 if(artist.name === hits.primary_artist.name){
+//                     artist.hits.push(hits)
+//                 }
+//                 artist.save()
+//             })
+//         })
+//     })
+// })
 
         
-
 
 // Artist.find({}).then( artist => artist.forEach(artist => {
 //     youtube[artist.name].forEach(link => Youtube.deleteMany({})
--- db/seed/youtube.js
+++ db/seed/youtube.js
@@ -4,15 +4,31 @@
 const axios = require('axios')
 const fs = require('fs')
 const Artist = require('../../models/Artists')
-const apiKey = ""
+const apiKey = "AIzaSyDeuyKWqo0zDyHhjXKY3VLQa0KcxXR-aAs"
+const apiKeyTwo = 'AIzaSyA1KZ99wXWVjT3ZlXmBhsewNG6-4WW1y-M'
+const apiKeyThree = 'AIzaSyBt3Cnclgw5zEOynnD4Fg-OLKnFJF7x-lU'
+const apiKeyFour = 'AIzaSyA3OREP-dYQK5IEJ9goW9Sa2FYRgW4xC2g'
+const apiKeyFive = 'AIzaSyD07U-Rb7WVBePIJwW-JnorJg2QEg2T6zs'
+let youtubeList = []
 
 
-// have to get each artist from Artist database
-Artist.find({}).then(artists => artists.forEach(artist =>{
-    let query = `${artist.name}%20concert`
-    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}-aAs`
-    axios.get(url).then(response => {
-        console.log(JSON.stringify({"${artist.name}":response.data.items[0].id.videoId}))
-    })
-}))
+
+
+
+    // have to get each artist from Artist database
+    Artist.find({}).then(artists => artists.forEach(artist =>{
+        let query = `${artist.name}%20concert`
+        let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKeyFive}`
+        axios.get(url).then(response => {
+            response.data.items[0].artist = `${artist.name}`
+            youtubeList.push(response.data)
+        })
+        // .then(()=> console.log(JSON.stringify(youtubeList))).catch(e => console.log(e))
+        .then(()=> fs.writeFile('youtube-ids-2.json',JSON.stringify(youtubeList),(err)=>{
+            if(err){
+                console.log(err)
+            }
+        })).catch(e => console.log(e))
+    }))
+
 
