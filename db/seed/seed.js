ff83b8ad9e3ee3357e3893b63716c5ede3e72681
Jerry Davidson <willdjerry@gmail.com>
2019-09-02 13:51:23 -0400

    youtube links work

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -11,14 +11,59 @@ const youtube = require("./youtube-ids-2.json");
 
 const Genre = require("../../models/Genre");
 
-// youtube.forEach((response, i = 0) => {
-//   console.log(response.items[0].artist, `${i}`);
-//   response.items.forEach((item, i = 0) => {
-//     console.log(item.id.videoId, `${i}`);
-//     return i + 1;
-//   });
-// });
 
+
+Artist.deleteMany({})
+  .then(Artist.create(artists))
+  .finally(() => console.log("done seeding artists"));
+
+Hits.find({})
+  .then(
+    Hits.deleteMany({}).then(
+      hits.forEach(hit =>
+        hit.forEach(hitSong => {
+          Hits.create(hitSong.result);
+        })
+      )
+    )
+  )
+  .finally(console.log("done adding songs"));
+
+Artist.find({}).then(artists => {
+  Hits.find({}).then(hit => {
+    artists.forEach(artist => {
+      hit.forEach(hits => {
+        if (artist.name === hits.primary_artist.name) {
+          artist.hits.push(hits);
+        }
+        artist.save();
+      });
+    });
+  });
+});
+
+Artist.find({}).then(artist =>
+  artist.forEach(artist => {
+    youtube[artist.name].forEach(link =>
+      Youtube.deleteMany({}).then(
+        Youtube.create({
+          link: `https://www.youtube.com/watch?v=${link.id.videoId}`,
+          name: artist.name
+        })
+      )
+    );
+  })
+);
+
+Genre.find({}).then(
+  Genre.deleteMany({}).then(
+    artists.forEach(artist => {
+      artist.classifications.forEach(classification => {
+        Genre.create({ genre: classification.genre.name, name: artist.name });
+      });
+    })
+  )
+);
 Youtube.find({}).then(
   youtube.forEach(response => {
     response.items.forEach(item =>
@@ -29,38 +74,3 @@ Youtube.find({}).then(
     );
   })
 );
-
-// Artist.deleteMany({}).then(Artist.create(artists)).finally(()=> console.log('done seeding artists'))
-
-// Hits.find({}).then(
-//   Hits.deleteMany({}).then(
-//     hits.forEach(hit =>
-//       hit.forEach(hitSong =>{Hits.create(hitSong.result)
-//       })
-//     )
-//   )
-// ).finally(console.log('done adding songs'))
-
-// Artist.find({}).then(artists =>{
-//     Hits.find({}).then(hit => {
-//         artists.forEach(artist =>{
-//             hit.forEach(hits =>{
-//                 if(artist.name === hits.primary_artist.name){
-//                     artist.hits.push(hits)
-//                 }
-//                 artist.save()
-//             })
-//         })
-//     })
-// })
-
-// Artist.find({}).then( artist => artist.forEach(artist => {
-//     youtube[artist.name].forEach(link => Youtube.deleteMany({})
-//     .then(Youtube.create({link:`https://www.youtube.com/watch?v=${link.id.videoId}`,name: artist.name})))
-// }))
-
-// Genre.find({}).then(Genre.deleteMany({}).then(artists.forEach(artist =>{
-//     artist.classifications.forEach(classification => {
-//         Genre.create({genre: classification.genre.name, name: artist.name})
-//     })
-// })))
