352cb4920f2d377eeb578c1945aa519ab08a4044
Jerry Davidson <willdjerry@gmail.com>
2019-09-03 13:48:56 -0400

    artists switch to artists2.json gets the genres

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -3,6 +3,8 @@ const mongoose = require("../connection");
 const Artist = require("../../models/Artists");
 let artists = require("./all-genius-artists.json");
 
+let artists2 = require("./artists.json")
+
 const Hits = require("../../models/Hits");
 const hits = require("./genius-artists-hits.json");
 
@@ -13,64 +15,64 @@ const Genre = require("../../models/Genre");
 
 
 
-Artist.deleteMany({})
-  .then(Artist.create(artists))
-  .finally(() => console.log("done seeding artists"));
+// Artist.deleteMany({})
+//   .then(Artist.create(artists))
+//   .finally(() => console.log("done seeding artists"));
 
-Hits.find({})
-  .then(
-    Hits.deleteMany({}).then(
-      hits.forEach(hit =>
-        hit.forEach(hitSong => {
-          Hits.create(hitSong.result);
-        })
-      )
-    )
-  )
-  .finally(console.log("done adding songs"));
+// Hits.find({})
+//   .then(
+//     Hits.deleteMany({}).then(
+//       hits.forEach(hit =>
+//         hit.forEach(hitSong => {
+//           Hits.create(hitSong.result);
+//         })
+//       )
+//     )
+//   )
+//   .finally(console.log("done adding songs"));
 
-Artist.find({}).then(artists => {
-  Hits.find({}).then(hit => {
-    artists.forEach(artist => {
-      hit.forEach(hits => {
-        if (artist.name === hits.primary_artist.name) {
-          artist.hits.push(hits);
-        }
-        artist.save();
-      });
-    });
-  });
-});
+// Artist.find({}).then(artists => {
+//   Hits.find({}).then(hit => {
+//     artists.forEach(artist => {
+//       hit.forEach(hits => {
+//         if (artist.name === hits.primary_artist.name) {
+//           artist.hits.push(hits);
+//         }
+//         artist.save();
+//       });
+//     });
+//   });
+// });
 
-Artist.find({}).then(artist =>
-  artist.forEach(artist => {
-    youtube[artist.name].forEach(link =>
-      Youtube.deleteMany({}).then(
-        Youtube.create({
-          link: `https://www.youtube.com/watch?v=${link.id.videoId}`,
-          name: artist.name
-        })
-      )
-    );
-  })
-);
+// Artist.find({}).then(artist =>
+//   artist.forEach(artist => {
+//     youtube[artist.name].forEach(link =>
+//       Youtube.deleteMany({}).then(
+//         Youtube.create({
+//           link: `https://www.youtube.com/watch?v=${link.id.videoId}`,
+//           name: artist.name
+//         })
+//       )
+//     );
+//   })
+// );
 
 Genre.find({}).then(
   Genre.deleteMany({}).then(
-    artists.forEach(artist => {
+    artists2.forEach(artist => {
       artist.classifications.forEach(classification => {
         Genre.create({ genre: classification.genre.name, name: artist.name });
       });
     })
   )
 );
-Youtube.find({}).then(
-  youtube.forEach(response => {
-    response.items.forEach(item =>
-      Youtube.create({
-        link: `https://www.youtube.com/watch?v=${item.id.videoId}`,
-        name: response.items[0].artist
-      })
-    );
-  })
-);
+// Youtube.find({}).then(
+//   youtube.forEach(response => {
+//     response.items.forEach(item =>
+//       Youtube.create({
+//         link: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+//         name: response.items[0].artist
+//       })
+//     );
+//   })
+// );
