aaad7e5b19e667cba2a972c8d8c40147a29b242a
Jerry Davidson <jerrydavidson@Jerrys-MBP-2.home>
2021-10-08 19:20:55 -0400

    update hits JSON loop

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,60 +1,60 @@
-const mongoose = require("../connection");
+import ArtistModel from "../../models/Artists.js";
+import * as artists from "./all-genius-artists.json";
 
-const Artist = require("../../models/Artists");
-let artists = require("./all-genius-artists.json");
+import HitsModel from "../../models/Hits.js";
+import * as hits from "./genius-artists-hits.json";
+console.log(hits);
 
+import YoutubeModel from "../../models/Youtube.js";
+import * as youtube from "./youtube-ids-2.json";
 
-const Hits = require("../../models/Hits");
-const hits = require("./genius-artists-hits.json");
-
-const Youtube = require("../../models/Youtube");
-const youtube = require("./youtube-ids-2.json");
-
-
-
-
-Artist.deleteMany({})
-  .then(Artist.create(artists))
-  .then( console.log("done seeding artists")).then(
-    Hits.find({})
+ArtistModel.deleteMany({})
+  .then(ArtistModel.create(artists))
+  .then(console.log("done seeding artists"))
+  .then(
+    HitsModel.find({})
       .then(
-        Hits.deleteMany({}).then(
-          hits.forEach(hit =>
-            hit.forEach(hitSong => {
-              Hits.create(hitSong.result);
+        HitsModel.deleteMany({}).then(
+          hits.default.forEach((hit) =>
+            hit.forEach((hitSong) => {
+              HitsModel.create(hitSong.result);
             })
           )
         )
       )
       .then(console.log("done adding songs"))
-  ).then(
-    Artist.find({}).then(artists => {
-      Hits.find({}).then(hits => {
-        artists.forEach(artist => {
-          hits.forEach(hit => {
-            if (artist.name === hit.primary_artist.name) {
-              artist.hits.push(hit);
-            }
-          })
-          artist.save().catch(e => console.log(e))
-        });
-      });
-    }).then(console.log('done seeding artist and hits refs'))
-  ).then(
-    Artist.find({}).then(artist =>
-      artist.forEach(artist => {
-        youtube[artist.name].forEach(link =>
-          Youtube.deleteMany({}).then(
-            Youtube.create({
-              link: `https://www.youtube.com/watch?v=${link.id.videoId}`,
-              name: artist.name
-            })
-          )
-        );
-      })
-    ).then(console.log('done seeding youtube links'))
   )
-  .finally(console.log('ALL SEEDING COMPLETED'))
-
-  
-
+  .then(
+    ArtistModel.find({})
+      .then((artists) => {
+        HitsModel.find({}).then((hits) => {
+          artists.forEach((artist) => {
+            hits.forEach((hit) => {
+              if (artist.name === hit.primary_artist.name) {
+                artist.hits.push(hit);
+              }
+            });
+            artist.save().catch((e) => console.log(e));
+          });
+        });
+      })
+      .then(console.log("done seeding artist and hits refs"))
+  )
+  .then(
+    ArtistModel.find({})
+      .then((artist) =>
+        artist.forEach((artist) => {
+          youtube[artist.name].forEach((link) => {
+            console.log(link);
+            YoutubeModel.deleteMany({}).then(
+              YoutubeModel.create({
+                link: `https://www.youtube.com/watch?v=${link.id.videoId}`,
+                name: artist.name,
+              })
+            );
+          });
+        })
+      )
+      .then(console.log("done seeding youtube links"))
+  )
+  .finally(console.log("ALL SEEDING COMPLETED"));
