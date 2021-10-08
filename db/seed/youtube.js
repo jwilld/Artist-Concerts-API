1e2687af4ab593421335d00aade8940ac336f2ef
Jerry Davidson <jerrydavidson@Jerrys-MacBook-Pro-2.local>
2021-10-08 12:33:26 -0400

    update package vulnerabilities

--- db/seed/youtube.js
+++ db/seed/youtube.js
@@ -1,30 +1,35 @@
 // format for youtube links
-// 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=QUERY&key=[YOUR_API_KEY]' 
+// 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=QUERY&key=[YOUR_API_KEY]'
 
-const axios = require('axios')
-const fs = require('fs')
-const Artist = require('../../models/Artists')
+import axios from "axios";
+const fs = require("fs");
+const Artist = require("../../models/Artists");
 
-let youtubeList = []
+let youtubeList = [];
 
-
-
-
-
-    // have to get each artist from Artist database
-    Artist.find({}).then(artists => artists.forEach(artist =>{
-        let query = `${artist.name}%20concert`
-        let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKeyFour}`
-        axios.get(url).then(response => {
-            response.data.items[0].artist = `${artist.name}`
-            youtubeList.push(response.data)
-        })
-        // .then(()=> console.log(JSON.stringify(youtubeList))).catch(e => console.log(e))
-        .then(()=> fs.writeFile('youtube-ids-3.json',JSON.stringify(youtubeList),(err)=>{
-            if(err){
-                console.log(err)
+// have to get each artist from Artist database
+Artist.find({}).then((artists) =>
+  artists.forEach((artist) => {
+    let query = `${artist.name}%20concert`;
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKeyFour}`;
+    axios
+      .get(url)
+      .then((response) => {
+        response.data.items[0].artist = `${artist.name}`;
+        youtubeList.push(response.data);
+      })
+      // .then(()=> console.log(JSON.stringify(youtubeList))).catch(e => console.log(e))
+      .then(() =>
+        fs.writeFile(
+          "youtube-ids-3.json",
+          JSON.stringify(youtubeList),
+          (err) => {
+            if (err) {
+              console.log(err);
             }
-        })).catch(e => console.log(e)) 
-    }))
-
-
+          }
+        )
+      )
+      .catch((e) => console.log(e));
+  })
+);
--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use('/',hitsController)
 
 
 
-app.listen(8080, () => console.log("They see me rollin...on port 8080..."));
 app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
